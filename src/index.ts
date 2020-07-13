10be1d5b69163841322cbe95252f2994af02929b
Dmitry Vasiliev <vasiliev.dim2014@gmail.com>
2020-07-13 12:25:28 +0300

    Add template engine.
    Signed-off-by: Dmitry Vasiliev <vasiliev.dim2014@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import * as express from "express";
+import * as path from "path";
+import * as exphbs from "express-handlebars";
 
 const app = express();
+
+app.set('views', path.join(__dirname, '/views'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/", (req, res) => {
-    res.send("hello world!");
+    res.render("index", { layout: false });
 });
 
 app.listen(3000, () => {
