80ef2cfd76d4ec3406554f3a6d5b6476e2e858cb
Dmitry Vasiliev <vasiliev.dim2014@gmail.com>
2020-07-13 15:13:28 +0300

    Add jest tests for frontend.
    Signed-off-by: Dmitry Vasiliev <vasiliev.dim2014@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
-  testRegex: '/src/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  testRegex: '(/src/tests/|/src/client/tests/).*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
