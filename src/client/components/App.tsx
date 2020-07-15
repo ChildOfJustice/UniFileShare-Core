8908e1f6a5e88328768c18855e1fc6ad47898217
Dmitry Vasiliev <vasiliev.dim2014@gmail.com>
2020-07-15 19:51:31 +0300

    Add react-bootstrap.
    Signed-off-by: Dmitry Vasiliev <vasiliev.dim2014@gmail.com>

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,56 +1,54 @@
-import * as React from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import * as React from 'react';
+import { MemoryRouter, Switch, Route } from 'react-router-dom';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default function App() {
-    return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/users">Users</Link>
-                        </li>
-                    </ul>
-                </nav>
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-                <Switch>
-                    <Route path="/about">
-                        <About />
-                    </Route>
-                    <Route path="/users">
-                        <Users />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
-}
+const Home = () => <span>Home</span>;
 
-function Home() {
-    return <h2>Home</h2>;
-}
+const About = () => <span>About</span>;
 
-function About() {
-    return <h2>About</h2>;
-}
+const Users = () => <span>Users</span>;
 
-function Users() {
-    return <h2>Users</h2>;
-}
+const App = () => (
+    <MemoryRouter>
+        <Container className="p-3">
+            <Jumbotron>
+                <h1 className="header">Welcome To React-Bootstrap</h1>
+                <h2>
+                    Current Page is{' '}
+                    <Switch>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/users">
+                            <Users />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </h2>
+                <h2>
+                    Navigate to{' '}
+                    <ButtonToolbar className="custom-btn-toolbar">
+                        <LinkContainer to="/">
+                            <Button>Home</Button>
+                        </LinkContainer>
+                        <LinkContainer to="/about">
+                            <Button>About</Button>
+                        </LinkContainer>
+                        <LinkContainer to="/users">
+                            <Button>Users</Button>
+                        </LinkContainer>
+                    </ButtonToolbar>
+                </h2>
+            </Jumbotron>
+        </Container>
+    </MemoryRouter>
+);
+
+export default App;
