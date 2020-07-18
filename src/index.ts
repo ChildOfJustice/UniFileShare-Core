146644880c51eaf869465f1339a58c00f3df0a2e
Dmitry Vasiliev <vasiliev.dim2014@gmail.com>
2020-07-18 20:48:23 +0300

    Server favicon.ico.
    Signed-off-by: Dmitry Vasiliev <vasiliev.dim2014@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as exphbs from "express-handlebars";
 import * as compression from "compression";
 import * as morgan from "morgan";
 import * as bodyParser from "body-parser";
+import * as serveFavicon from "serve-favicon";
 
 const app = express();
 
@@ -21,6 +22,8 @@ app.use(compression());
 app.use(morgan("common"));
 // serve json requests
 app.use(bodyParser.json());
+// serve favicon
+app.use(serveFavicon(path.join(process.cwd(), '/src', '/public', 'favicon.ico')))
 
 const PORT = process.env.PORT || 3000;
 
