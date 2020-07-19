947e702ecf1df9d6603fbb8e8042119832a1d0e7
Dmitry Vasiliev <vasiliev.dim2014@gmail.com>
2020-07-19 10:10:09 +0300

    Set connect to database.
    Signed-off-by: Dmitry Vasiliev <vasiliev.dim2014@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ import * as compression from "compression";
 import * as morgan from "morgan";
 import * as bodyParser from "body-parser";
 import * as serveFavicon from "serve-favicon";
+import { Sequelize } from "sequelize";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import * as config from "../util/config.js";
 
 const app = express();
 
@@ -27,10 +32,25 @@ app.use(serveFavicon(path.join(process.cwd(), '/src', '/public', 'favicon.ico'))
 
 const PORT = process.env.PORT || 3000;
 
+const sequelize = new Sequelize(config.db.name, config.db.username, config.db.password, {
+    host: config.db.host,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: { rejectUnauthorized: false }
+    }
+});
+
 app.use("/", (req, res) => {
     res.render("index");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+
     console.log(`Server started on http://localhost:${PORT}.`);
 });
