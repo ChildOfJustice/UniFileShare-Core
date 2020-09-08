2f440a2367b1a13e2b02cacae2524454c399d68e
Sardor <izzvms@gmail.com>
2020-09-08 10:40:17 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,26 @@
-import * as express from "express";
-import * as path from "path";
-import * as exphbs from "express-handlebars";
-import * as compression from "compression";
-import * as morgan from "morgan";
+import App from './app';
+import config from "../util/config";
+
 import * as bodyParser from "body-parser";
-import * as serveFavicon from "serve-favicon";
-import * as cors from "cors";
 
+import AuthController from "./controllers/auth.controller";
+import HomeController from './controllers/home.controller';
+import ProtectedController from "./controllers/protected.controller";
 
-import db from "./models"
-import routes from "./routes/test.routes"
-
-const app = express();
-
-// set static path
-app.use("/static", express.static("src/public"));
-//app.use(cors())
-
-// set template engine
-app.set('views', path.join(process.cwd(), '/src', '/views'));
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-// set response compression
-app.use(compression());
-// set logger
-app.use(morgan("common"));
-// serve json requests
-app.use(bodyParser.json());
-// serve favicon
-app.use(serveFavicon(path.join(process.cwd(), '/src', '/public', 'favicon.ico')))
-
-app.use(express.json());
-
-const PORT = process.env.PORT || 3001;
-
-app.use("/", (req, res) => {
+const app = new App({
+    port: config.server.port,
+    controllers: [
+        new ProtectedController(),
+        new HomeController(),
+        new AuthController()
+    ],
+    middleWares: [
+        bodyParser.json(),
+        bodyParser.urlencoded({ extended: true })
+    ],
+})
+//I cant make this "use" in routes, I dont know why
+app.app.use("/", (req, res) => {
     res.render("index");
 });
-
-app.listen(PORT, async () => {
-    // try {
-    //     await db.sequelizeEntity.authenticate();
-    //     console.log('Connection has been established successfully.');
-    //     //db.sequelizeEntity.sync();
-    //     //for development:
-    //     db.sequelizeEntity.sync({ force: true }).then(() => {
-    //         console.log("Drop and re-sync db.");
-    //     });
-    //     //^
-    //     routes(app);
-    // } catch (error) {
-    //     console.error('Unable to connect to the database: ', error);
-    // }
-
-    console.log(`Server started on http://localhost:${PORT}.`);
-});
+app.listen()
