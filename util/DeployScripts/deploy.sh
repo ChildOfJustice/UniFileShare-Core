37ea4a1eaa47cf8805d6962cc5a38ab2b0308c96
ChildOfJustice <izzvms@gmail.com>
2020-12-02 10:20:11 +0300

    Fixed deployment process.

--- util/DeployScripts/deploy.sh
+++ util/DeployScripts/deploy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-#echo 'export PATH="/home/sardor/.ebcli-virtual-env/executables:$PATH"' >> ~/.bash_profile && source ~/.bash_profile
-sh ./util/GenerateConfigFile/generateConfigScript.sh
-sh ./util/DeployScripts/dist.sh
-eb create --single
+#echo 'export PATH="/home/sardor/.ebcli-virtual-env/executables:$PATH"' >> ~/.bashrc
+echo "generating config file...";
+sh ./util/GenerateConfigFile/generateConfigScript.sh;
+
+echo "creating artifact...";
+sh ./util/DeployScripts/dist.sh;
+
+echo "executing elastic beanstalk create command...";
+eb create --single;
+
+echo "waiting app to create the database tables...";
+sleep 10;
+
+echo "publishing rest database infrastructure...";
 sh ./util/SqlScripts/publish.sh
+
